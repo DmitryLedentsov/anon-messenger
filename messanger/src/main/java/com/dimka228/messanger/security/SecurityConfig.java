9e6c09d063f45f97f00a0e35f753f23cbbc1eab5
DimaLedentsov <ledentd@mail.ru>
2023-11-01 02:36:51 +0300

    делаем вход

--- messanger/src/main/java/com/dimka228/messanger/security/SecurityConfig.java
+++ messanger/src/main/java/com/dimka228/messanger/security/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/index","/error").permitAll()
+                .requestMatchers("/auth/**","/error").permitAll()
                 .anyRequest().authenticated()
         ).formLogin(form -> form
                         .loginPage("/auth/login")
