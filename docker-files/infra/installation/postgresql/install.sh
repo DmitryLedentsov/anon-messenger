9163aaa73e4a8f6fccdcc180c9b8e525e60633f0
PotatoCoder228 <sasha.atalyan@gmail.com>
2024-11-24 04:20:52 +0300

    Fix code-style, postgres configuration and names

--- docker-files/infra/installation/postgresql/install.sh
+++ docker-files/infra/installation/postgresql/install.sh
@@ -2,13 +2,16 @@
 set -x
 
 apt-get install -y postgresql
-
-export APPPATH=${INSTALLPATH}/${1}
+export SERVICEPATH=/etc/main_service
+export INSTALLPATH=${SERVICEPATH}/infra/installation
+export BINPATH=${SERVICEPATH}/bin
+export APPPATH=${INSTALLPATH}/postgresql
 
 pg_createcluster -p 5435 15 messenger -- --auth=md5 --username=postgres --pwfile=${APPPATH}/pwfile.conf
 
 cat ${APPPATH}/pg_hba.conf > /etc/postgresql/15/messenger/pg_hba.conf
-cat ${APPPATH}/postgresql.conf > /etc/postgresql/15/messenger/postgresql.conf
 pg_ctlcluster 15 messenger start
-su -c  "psql -U postgres -p 5435 -f ${APPPATH}/scripts/create.sql" postgres
+su -c  "psql -U postgres -d postgres -p 5435 -f ${APPPATH}/scripts/create.sql" postgres
 pg_ctlcluster 15 messenger stop
+
+cat ${APPPATH}/postgresql.conf > /etc/postgresql/15/messenger/postgresql.conf
