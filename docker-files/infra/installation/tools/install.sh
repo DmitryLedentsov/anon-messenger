c15cb46528f33ce99cca9df2fd58e957f472a908
Alexander Atalyan <sasha.atalyan@gmail.com>
2024-12-07 21:46:59 +0300

    Fix docker-compose.yml

--- docker-files/infra/installation/tools/install.sh
+++ docker-files/infra/installation/tools/install.sh
@@ -2,5 +2,5 @@
 set -x
 
 apt-get install -y htop iotop-c 
-apt-get install -y selinux-basics selinux-policy-default auditd ecryptfs-utils
+# apt-get install -y selinux-basics selinux-policy-default auditd ecryptfs-utils
 apt-get install -y net-tools iptraf-ng iputils-ping iproute2 nftables
