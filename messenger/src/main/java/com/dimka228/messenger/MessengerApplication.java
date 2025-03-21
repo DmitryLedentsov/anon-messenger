b141ba3bc2a21e5780b6c046917358c50d2b4943
dimka_228 <ledentd@mail.ru>
2025-03-22 02:20:43 +0300

    еще немного конфига СД

--- messenger/src/main/java/com/dimka228/messenger/MessengerApplication.java
+++ messenger/src/main/java/com/dimka228/messenger/MessengerApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@EnableScheduling
-@EnableDiscoveryClient
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
+@EnableScheduling
+
 public class MessengerApplication {
 
     public static void main(String[] args) {
